d36ec063656784fe7946f1615b8576d7144aae74
Ana <anna.cristinna.2008@gmal.com>
2025-04-07 11:59:11 -0300

    crear pagina con links

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -15,3 +15,26 @@ def chau():
 def sxn(nombre):
     return f"<p>Hola {nombre}</p>"
 
+@app.route('/tirar-dado/int:caras>')
+def dado(caras):
+    from random import randint
+    n = randint(1,caras)
+    return f"<p>Tire un dado de (caras) caras, salio (n) </p>"
+
+
+
+@app.route('/')
+def main():
+    url_hola = url_for("hello")
+    url_dado = url_for("dado", caras=6)
+    url_logo = url_for("static", filename="img/logostar.png")
+
+    return f"""
+    <a rhef="{url_hola}">Hola</a>
+    <br>
+    <a href="{url_for("bye")}">Chau</a>
+    <br>
+    <a href="{url_logo}">Logo</a>
+    <br>
+    <a href="{url_dado}">Tirar_dado</a>
+    """
